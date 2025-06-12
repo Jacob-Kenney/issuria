023e6e4c45382e7e4cc01b8b822dd09a131738ff
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-06-12 14:02:37 +0100

    fix: auth fixed

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,16 +5,7 @@ import Google from "next-auth/providers/google"
 import Resend from "next-auth/providers/resend"
 import { SupabaseAdapter } from "@auth/supabase-adapter"
 
-// Check env variables
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY || !process.env.AUTH_GOOGLE_ID || !process.env.AUTH_GOOGLE_SECRET || !process.env.AUTH_RESEND_KEY) {
-    throw new Error('Missing required auth environment variables:\nSUPBASE_URL\nSUPABASE_KEY\nAUTH_RESEND_KEY\nGOOGLE_CLIENT_ID\nGOOGLE_CLIENT_SECRET');
-}
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL,
-    secret: process.env.SUPABASE_KEY,
-  }),
   providers: [
     Google, 
     Resend({
@@ -22,6 +13,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       from: "onboarding@resend.dev",
     })
   ],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL as string,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+  }),
   pages: {
     signIn: "/login",
   }
