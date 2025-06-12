ef2f86d527a68a0d5f51c58410f264f96904ccd0
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-06-12 14:09:43 +0100

    fix: swapped from supabase to redis auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,9 +3,16 @@ import Google from "next-auth/providers/google"
 // import Apple from "next-auth/providers/apple" // Not free (need developer program)
 // import Twitter from "next-auth/providers/twitter" // Need twitter account
 import Resend from "next-auth/providers/resend"
-import { SupabaseAdapter } from "@auth/supabase-adapter"
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter"
+import { Redis } from "@upstash/redis"
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL!,
+  token: process.env.UPSTASH_REDIS_TOKEN!,
+})
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: UpstashRedisAdapter(redis),
   providers: [
     Google, 
     Resend({
@@ -13,10 +20,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       from: "onboarding@resend.dev",
     })
   ],
-  adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL as string,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
-  }),
   pages: {
     signIn: "/login",
   }
